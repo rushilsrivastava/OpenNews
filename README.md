7a92094c3b318a49ba78735ece4ea9d8cff899eb
Rushil Srivastava <rushu0922@gmail.com>
2018-08-15 23:42:38 -0700

    release v0.01

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# OpenNews - A Chrome extension blocking PayWalls on News Sites
+####Democracy dies behind paywalls.
+A simple chrome extension that helps making the navigating the web and Google News easier! Adding more sites on a request basis, feel free to send a PR. I cannot publish this on the Web Store as it is a violation of their terms and conditions. 
+
+I made this because as a student, it isn't practical for me to pay each company tremendous amount of money yearly for their articles. The new PayWall concept just doesn't work for the average student. As always, pay if you can, because you are what keeps Journalism alive. 
+
+
+### Supported Websites:
+*   [Wall Street Journal](https://www.wsj.com/)
+*   [New York Times](https://www.nytimes.com/)
+*   [Financial Times](https://www.ft.com/)
+*   [Bloomberg](https://www.bloomberg.com/)
+*   [Washington Post](https://www.washingtonpost.com)
+*   [Business Journals](https://www.bizjournals.com/)
+*   [Philly.com](https://www.philly.com/)
+*   [Kleine Zeitung](http://www.kleinezeitung.at/)
+*   [The Globe and Mail](https://www.theglobeandmail.com/)
+*   [New York Daily News](http://www.nydailynews.com/)
+*   [Mercury News](https://www.mercurynews.com/)
+
---
+++ src/js/background.js
@@ -0,0 +1,129 @@
+// new http header parameters to override
+const newHeader = {
+  referer: {
+    name: "Referer",
+    value: "https://www.facebook.com", // or "https://www.twitter.com"
+  },
+  cookie: {
+    name: "Cookie",
+    value: ""
+  },
+  cachecontrol: {
+    name: "Cache-Control",
+    value: "max-age=0"
+  }
+};
+
+// sites that we want to access
+const sites = {
+  washingtonpost: {
+    js: [
+      "*://*.washingtonpost.com/*pwapi/*.js*", // this one causes paywall/ad-wall lightbox for every article
+      "*://*.washingtonpost.com/*drawbridge/drawbridge.js?_*", // this one causes paywall/ad-wall lightbox sometimes with Adblock Plus enabled
+    ]
+  },
+  wsj: {
+    url: "*://*.wsj.com/*",
+    js: [
+      "*://*/*cxense-candy.js", // this one causes a pop up advertisement for every article
+    ]
+  },
+  ft: {
+    url: "*://*.ft.com/*",
+  },
+  nyt: {
+    js: [
+      "*://*.com/*mtr.js", // this one causes a pop up asking for subscription
+    ]
+  },
+  bloomberg: {
+    url: "*://*.bloomberg.com/*",
+    js: [
+      "*://*.bwbx.io/s3/javelin/public/javelin/js/pianola/*",
+    ]
+  },
+  bizjournals: {
+    url: "*://*.bizjournals.com/*",
+    js: [
+      "*://*.bizjournals.com/dist/js/article.min.js*"
+    ]
+  },
+  philly: {
+    url: "*://*.philly.com/*",
+  },
+  kleinezeitung: {
+    url: "*://*.kleinezeitung.at/*",
+  },
+  globeandmail: {
+    js: [
+      "*://*.theglobeandmail.com/pb/gr/c/default/*/story-bundle/*.js*"
+    ]
+  },
+  nydailynews: {
+    url: "*://*.nydailynews.com/*",
+    js: [
+      "*://*.tribdss.com/reg/tribune/*"
+    ]
+  },
+  mercurynews: {
+    url: "*://*.mercurynews.com/*",
+    js: [
+      "*://*.mercurynews.com/_static/*"
+    ]
+  }
+};
+
+// extract all script urls we want to block
+var script_urls = Object.values(sites)
+  .map(site => site.js)
+  .filter(Array.isArray)
+  .reduce((prev, curr) => prev.concat(curr), []);
+
+// extract all main_frame urls we want to override
+var main_frame_urls = Object.values(sites)
+  .map(site => site.url)
+  .filter(url => url);
+
+// add Firefox and Edge support with the global `browser` object
+browser = typeof browser !== "undefined" ? browser : chrome;
+
+browser.webRequest.onBeforeRequest.addListener(
+  function() {
+    console.log("OpenNews [DEBUG]: Blocking Paywall Javascripts");
+
+    return {
+      cancel: true
+    };
+  }, {
+    urls: script_urls,
+    // target is script
+    types: ["script"]
+  }, ["blocking"]
+);
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    console.log("OpenNews [DEBUG]: Modifying Request Headers");
+
+    // remove existing referer and cookie
+    for (let i = 0; i < details.requestHeaders.length; i++) {
+      if (details.requestHeaders[i].name === newHeader.referer.name || details.requestHeaders[i].name === newHeader.cookie.name) {
+        details.requestHeaders.splice(i, 1);
+        i--;
+      }
+    }
+
+    // add new referer
+    details.requestHeaders.push(newHeader.referer);
+    // remove cache
+    details.requestHeaders.push(newHeader.cachecontrol);
+
+    return {
+      requestHeaders: details.requestHeaders
+    };
+  }, {
+    urls: main_frame_urls,
+    // target is the document that is loaded for a top-level frame
+    types: ["main_frame"]
+  }, ["blocking", "requestHeaders"]
+);
